3dd915a9755cc4dda115e53c1fb60285ec7a96e5
unknown <frp.plata@gmail.com>
2023-03-23 08:45:38 +0100

    bring the info from the localstorage

--- components/common/bigCard.tsx
+++ components/common/bigCard.tsx
@@ -15,13 +15,13 @@ const BigCard = ({ image, artist, author, comment }: CardProps) => {
         <img src={image} alt={image} />
       </div>
       <div className={styles.big_card_title}>
-        <p className={styles.big_card_artist}>{artist.substring(0, 20)}</p>
-        <p className={styles.big_card_author}>by: {author.substring(0, 20)} </p>
+        <p className={styles.big_card_artist}>{artist?.substring(0, 20)}</p>
+        <p className={styles.big_card_author}>by: {author?.substring(0, 20)} </p>
       </div>
       <div className={styles.big_card_description}>
         <span> Description:</span>
         <p>
-          {comment.substring(0, 80)}...
+          {comment?.substring(0, 80)}...
         </p>
       </div>
     </div>
--- pages/podcast/[podcastId]/mediaPlayer/[podcastPlayer].tsx
+++ pages/podcast/[podcastId]/mediaPlayer/[podcastPlayer].tsx
@@ -3,25 +3,33 @@ import BigCard from "@/components/common/bigCard";
 import { useSelector } from "react-redux";
 import styles from "@/styles/mediaPlayer.module.css";
 import MainHeader from "@/components/common/mainHeader";
+import { useEffect, useState } from "react";
 
 const PodcastPlayer = () => {
-  const router = useRouter();
   const selectedSinglePodcast = useSelector(
     (state: any) => state.itunesPodcast.selectedSinglePodcast
   );
-  return (
+  const [podcastCard, setPodcastCard] = useState(null);
+  useEffect(() => {
+    setPodcastCard((localStorage.getItem("podcasts") as unknown) as null);
+  }, []);
+  const dataPodcastCard = JSON.parse((podcastCard as unknown) as string);
+
+  const image = dataPodcastCard?.item["im:image"][2].label;
+  const artist = dataPodcastCard?.item["im:name"].label;
+  const author = dataPodcastCard?.item["im:artist"].label
+    .replace("Podcast", "")
+    .substring(0, 10);
+  const comment = dataPodcastCard?.item?.summary.label;
+   return (
     <>
       <MainHeader />
       <div className={styles.mediaPlayer}>
         <BigCard
-          image={selectedSinglePodcast.artworkUrl160}
-          artist={selectedSinglePodcast.collectionName}
-          author={selectedSinglePodcast.collectionName}
-          comment={
-            selectedSinglePodcast?.shortDescription
-              ? selectedSinglePodcast?.shortDescription
-              : selectedSinglePodcast?.description
-          }
+          image={image}
+          artist={artist}
+          author={author}
+          comment={comment}
         />
         <div className={styles.mediaPlayer_container}>
           <h1 className={styles.mediaPlayer_title}>
