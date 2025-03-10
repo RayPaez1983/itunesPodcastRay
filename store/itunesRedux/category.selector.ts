4555c6d6a2fddbd82c27a69ee7c0db3dc2932adf
RayPaez1983 <frp.plata@gmail.com>
2025-03-10 19:17:37 +0100

    small change to redeploy

--- store/itunesRedux/category.selector.ts
+++ store/itunesRedux/category.selector.ts
@@ -3,7 +3,7 @@ import { Podcasts } from '@/utils/type';
 export const itunesPodcastSelector = (state: {
   itunesPodcast: { itunesPodcast: any };
 }) => {
-  console.log(state, 'state here ');
+
   const data = state.itunesPodcast.itunesPodcast;
   return data;
 };
