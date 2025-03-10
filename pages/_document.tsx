7f818a6f96b5d81c1fbad5c78799a795451a651d
RayPaez1983 <frp.plata@gmail.com>
2025-03-10 19:49:05 +0100

    remove the minfied flag from _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  const assetPath =
-    process.env.NEXT_PUBLIC_ASSET_MODE === 'minified'
-      ? '/assets/main.min.js'
-      : '/assets/main.js';
+
   return (
     <Html>
-      <Head>
-        <script src={assetPath} async />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
